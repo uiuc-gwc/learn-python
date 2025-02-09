af6d9d77f215423ee79dfcec7888ba5cd1d01356
Ananya <14322650+ananyeet@users.noreply.github.com>
2025-02-09 09:53:42 -0600

    move to old

--- 3_strings.py
+++
@@ -1,6 +0,0 @@
-# When you want to use a word in python, it has to be a String. 
-# Strings must ALWAYS be in quotes. 
-
-# Try storing your favorite color in the following variable:
-favorite_color = 
-print(favorite_color)
