39e1d856786856e617636d13cdb711eabb6e3553
Ananya <14322650+ananyeet@users.noreply.github.com>
2025-02-09 10:20:13 -0600

    up to math

---
+++ 3_data_types.py
@@ -0,0 +1,5 @@
+# Try your own! 
+# Make a string representing your favorite food. 
+# Make an integer with your age. 
+# Make a float with your height.
+# Make a boolean that is either True or False. 
---
+++ 5_math.py
@@ -0,0 +1,8 @@
+# Write your age and your partner's age as two variables. 
+# Add them up. 
+# Print your added age. 
+
+
+
+# print whether you are older than your partner *using a boolean*
+
